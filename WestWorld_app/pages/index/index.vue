5c6d2c12ce3a24cba6a99bb6d37781afe1786f52
Greetme <1980900198@qq.com>
2019-10-22 18:17:48 +0800

    add WestWorld_app/pages/user/

--- WestWorld_app/pages/index/index.vue
+++ WestWorld_app/pages/index/index.vue
@@ -1,41 +1,47 @@
 <template>
-	<view class="page">
-		<view v-if="!hasLogin">现在是未登录状态，点击按钮进行登录</view>
-		<view v-else>现在是登录状态，您的用户token是：{{userInfo.token}}</view>
-		<button type="primary" @click="bindLogin">{{hasLogin ? '退出登录' : '登录'}}</button>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
 	</view>
 </template>
 <script>
 	import {mapState, mapMutations} from 'vuex';
 	
 	export default {
-		computed: mapState(['hasLogin', 'userInfo']), // 对全局变量 hasLogin、userInfo 进行监控
-		methods: {
-			...mapMutations(['logout']), // 对全局方法 logout 进行监控
-			bindLogin() {
-				if (this.hasLogin) {
-					this.logout()
-				} else {
-					uni.navigateTo({
-						url: '/pages/login/login'
-					})
-				}
+		data() {
+			return {
+				title: 'Hello'
 			}
 		}
 	}
 </script>
 
 <style>
-	.page{
-		padding: 50upx 30upx;
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
-	view{
-		line-height: 1.5;
-		font-size: 32upx;
+	
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
 	}
-	button{
-		margin-top: 30upx;
-		height: 80upx;
-		line-height: 80upx;
+	
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+	
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
 	}
 </style>
---
+++ WestWorld_app/pages/user/user.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="page">
+		<view v-if="!hasLogin">现在是未登录状态，点击按钮进行登录</view>
+		<view v-else>现在是登录状态，您的用户token是：{{userInfo.token}}</view>
+		<button type="primary" @click="bindLogin">{{hasLogin ? '退出登录' : '登录'}}</button>
+	</view>
+</template>
+<script>
+	import {mapState, mapMutations} from 'vuex';
+	
+	export default {
+		computed: mapState(['hasLogin', 'userInfo']), // 对全局变量 hasLogin、userInfo 进行监控
+		methods: {
+			...mapMutations(['logout']), // 对全局方法 logout 进行监控
+			bindLogin() {
+				if (this.hasLogin) {
+					this.logout()
+				} else {
+					uni.navigateTo({
+						url: '/pages/login/login'
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.page{
+		padding: 50upx 30upx;
+	}
+	view{
+		line-height: 1.5;
+		font-size: 32upx;
+	}
+	button{
+		margin-top: 30upx;
+		height: 80upx;
+		line-height: 80upx;
+	}
+</style>
