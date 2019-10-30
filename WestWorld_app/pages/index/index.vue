a822adab516349e208b96e621613a6e897801347
Greetme <1980900198@qq.com>
2019-10-30 17:43:19 +0800

    WestWorld_app: new index/search

--- WestWorld_app/pages/index/index.vue
+++ WestWorld_app/pages/index/index.vue
@@ -122,6 +122,31 @@
 			this.status = 'more';
 			this.getList();
 		},
+		
+		/**
+		 * 当 searchInput 配置 disabled 为 true 时触发
+		 */
+		onNavigationBarSearchInputClicked(e) {
+			console.log('事件执行了')
+			uni.navigateTo({
+				url: '/pages/index/search/search'
+			});
+		},
+		/**
+		 *  点击导航栏 buttons 时触发
+		 */
+		onNavigationBarButtonTap() {
+			uni.showModal({
+				title: '提示',
+				content: '用户点击了功能按钮，这里仅做展示。',
+				success: res => {
+					if (res.confirm) {
+						console.log('用户点击了确定');
+					}
+				}
+			});
+		},
+		
 		computed: mapState(['userInfo']), // 对全局变量 userInfo 进行监控
 		methods: {
 			/**
