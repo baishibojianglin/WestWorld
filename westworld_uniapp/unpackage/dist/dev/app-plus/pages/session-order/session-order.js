ef2bb9a90750dd27aaa85826bed0761c4ab02da6
Greetme <1980900198@qq.com>
2020-01-04 10:35:07 +0800

    delete westworld_uniapp/unpackage

--- westworld_uniapp/unpackage/dist/dev/app-plus/pages/session-order/session-order.js
+++
@@ -1,2 +0,0 @@
-setCssToHead([".",[1],"footer-box { display: -webkit-box; display: -webkit-flex; display: flex; -webkit-box-pack: end; -webkit-justify-content: flex-end; justify-content: flex-end; -webkit-box-orient: horizontal; -webkit-box-direction: normal; -webkit-flex-direction: row; flex-direction: row; }\n.",[1],"mini-btn { margin-right: ",[0,10],"; }\n",],undefined,{path:"./pages/session-order/session-order.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/session-order/session-order.wxml') } }));
